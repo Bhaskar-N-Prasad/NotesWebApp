9ffce8a5a4ff9691afa64668febe5b55d7e8bee3
Bhaskar-N-Prasad <119781809+Bhaskar-N-Prasad@users.noreply.github.com>
2023-05-06 13:08:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+let submit=document.querySelector(".submit");
+let notesElem=document.querySelector('.notes');
+let title=document.querySelector("#text");
+let desc=document.querySelector('#desc');
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
+    });
+}
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    let descVal=desc.value;
+    if(obj){
+        titleval=obj.title;
+        descVal=obj.desc;
+    }
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3>
+                                    <p class="ptag">${descVal}</p>
+                             <button class="del">Delete</button>`;
+        notesElem.appendChild(card);
+        updateLs()
+    }
+    let del=card.querySelector(".del");
+    del.addEventListener('click', ()=>{
+        card.remove();
+        updateLs();
+    })
+}
+function updateLs() {
+    let card=document.querySelectorAll(".card");
+    let arr=[];
+    card.forEach(element => {
+        arr.push({
+            title:element.children[0].innerText,
+            desc:element.children[1].innerText
+        })
+    });
+    localStorage.setItem("notes", JSON.stringify(arr));
+}
+search.addEventListener('keyup', () => {
+  let searchQuery = search.value.toLowerCase();
+  let cards = document.querySelectorAll('.card');
+
+  cards.forEach((card) => {
+    let title = card.querySelector('h3').innerText.toLowerCase();
+    let desc = card.querySelector('.ptag').innerText.toLowerCase();
+
+    if (title.includes(searchQuery) || desc.includes(searchQuery)) {
+      card.style.display = 'block';
+    } else {
+      card.style.display = 'none';
+    }
+  });
+});
